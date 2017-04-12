58f00e1186ef1c75d375006a239f4fbf4252fb71
lukasz f <lukasz@MacBook-lukasz.local>
2017-04-12 18:10:32 +0200

    pandas examples

--- pandas_pd.py
+++ pandas_pd.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 brics = pd.read_csv("brics.csv", index_col=0)  # Load csv file contains labels in first row
-print(brics)
-print('---')
-print(brics['country'])  # Column as Pandas Series
-print(brics[['country']])  # Column as Pandas DataFrame
-print(brics.country)  # Column access with dot notation
-print('---')
+brics
+
+brics['country']  # Column as Pandas Series
+brics[['country']]  # Column as Pandas DataFrame
+brics.country  # Column access with dot notation
+brics.get('country')  # Column access
+
 brics['on_earth'] = [True, True, True, True, True]  # Add column
 print(brics)
 print('---')
@@ -21,3 +22,19 @@ print(brics['capital'].loc['CH'])
 print(brics.loc['CH']['capital'])
 print('---')
 print(brics.loc[['BR', 'CH'], ['population', 'area']])  # Sub-DataFrame
+
+# Create DataFrame from dictionary
+frame_dict = pd.DataFrame(
+    {"A": [1, 2, 3, 4, 5],
+     "B": ["k", None, "k","m", "k"],
+     "C": [False, True, True, False, False]
+    })
+frame_dict
+
+# Create DataFrame from list with columns name and index name
+frame_array = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"], index=['a', 'b', 'c'])
+frame_array
+
+# Create Series
+series_array = pd.Series([1, 2, 3, 4])
+series_array
