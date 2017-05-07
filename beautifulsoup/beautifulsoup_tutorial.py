4634d1651ae1ea092aba42e186eaa97ffe881c17
lukasz-f <lukaszf6@gmail.com>
2017-05-07 15:29:52 +0200

    nowe mozliwosci bs4

--- beautifulsoup/beautifulsoup_tutorial.py
+++ beautifulsoup/beautifulsoup_tutorial.py
@@ -25,6 +25,7 @@ BeautifulSoup(open("index.html"))  # open file and parse with best available par
 print(soup.prettify())
 print(soup.get_text())
 
+# Find first
 soup.title  # <title>The Dormouse's story</title>
 soup.title.name  # title
 soup.title.string  # The Dormouse's story
@@ -32,7 +33,15 @@ soup.title.parent.name  # head
 soup.p  # <p class="title"><b>The Dormouse's story</b></p>
 soup.p['class']  # ['title']
 soup.a  # <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+soup.a.text  # Elsie
+soup.a['href']  # http://example.com/elsie
+soup.find('a')  # <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+soup.find(text='Lacie')  # 'Lacie'
 soup.find(id="link3")  # <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+soup.find(class_="sister")  # <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+
+# Find all
+soup('a')
 soup.find_all('a')
 # [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
 #  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
@@ -43,3 +52,14 @@ for link in soup.find_all('a'):
 # http://example.com/elsie
 # http://example.com/lacie
 # http://example.com/tillie
+
+soup(text='Lacie')  # ['Lacie']
+soup(id='link2')  # [<a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>]
+soup(class_='sister')
+# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
+#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
+#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
+
+soup.a.next_element  # 'Elsie'
+soup.a.next_sibling  # ',\n'
+soup.a.find_next_sibling('a')  # <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>
