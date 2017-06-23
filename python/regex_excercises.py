493ecf036de2ab3f64d253e18c551e1265af9fc3
lukasz-f <lukaszf6@gmail.com>
2017-06-23 22:06:23 +0200

    regex

--- python/regex_excercises.py
+++ python/regex_excercises.py
@@ -1,22 +1,12 @@
 import re
 import codecs
 
-regex = r'([a-zA-Z]+) (\d+)'
-strng = "June 24, August 9, Dec 12"
-print(re.sub(regex, r'\2 of \1', strng))
-
-print(re.findall(regex, strng))
-
-matchlist = re.finditer(regex, strng)
-for m in matchlist:
-    print(m.group(1), m.group(2))
-
 # Operacje należy przeprowadzić na pliku shakespeare.txt
 # 1. Wybrać wszystkie słowa rozpoczynające się wielką literą.
-with codecs.open('shakespeare.txt', 'r', 'utf8') as file:
+with codecs.open('../datasets/books/shakespeare.txt', 'r', 'utf8') as file:
     data = file.read()
     matchList = re.findall(r'\b[A-Z]\w+', data)
-    #print(matchList)
+    print(matchList)
 
 # 2. Wybrać słowa kończące zdanie. Za słowo kończące zdanie uznajemy taki po którym
 # następuje znak ., ? lub !
@@ -25,7 +15,7 @@ with codecs.open('shakespeare.txt', 'r', 'utf8') as file:
 
 # 3. Wybrać każde słowo występujące po słowie 'The'.
     matchList = re.findall(r'The\s(\w+)', data)
-    print(matchList)
+    #print(matchList)
 
 # 4. Wybrać każde słowo występujące po słowie 'The' jeśli rozpoczyna się wielką literą.
 """5. Wybrać wszystkie liczby całkowite z tekstu
